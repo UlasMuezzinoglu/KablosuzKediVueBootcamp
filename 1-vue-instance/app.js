9f93476cc644837bdcba5643ea3c1e7afaa952f1
UlasMuezzinoglu <batan_gem1@outlook.com>
2021-07-30 20:34:35 +0300

    1.ünite sonu

--- 1-vue-instance/app.js
+++ 1-vue-instance/app.js
@@ -1,16 +1,30 @@
 const app = Vue.createApp({
-    data(){
-        return {
-            title : "Vue.js Bootcamp 1.gün",
-            content : "Lorem ipsum dolor sit amet..." ,
-            eduflow : {
-                title: "Müfredat ve açıklamalar için tıklayınız",
-                target : "_blank",
-                url: "http://eduflow.kablosuzkedi.com",
-                alt : "müfredat-kablosuzkedi-vue-bootcamp",
-                owner : "pogaca"
-            },
-            
-        }
-    }
-}).mount("#app");
+    data() {
+      return {
+        title: "Vue.js Bootcamp 1.gün",
+        content: "Lorem ipsum dolor sit amet..",
+        x: "0",
+        y : "0",
+        eduflow: {
+          title: "Müfredat ve açıklamalar için tıklayınız",
+          target: "_blank",
+          url: "https://eduflow.kablosuzkedi.com",
+          alt: "mufredat-kablosuzkedi-vue-bootcamp",
+        },
+        owner: "Pogaca",
+        
+      };
+    },
+    methods: {
+      changeTitle(pTitle) {
+        this.title = pTitle;
+      },
+      updateCoords(event){
+         
+          
+
+          this.changeTitle(`${this.x = event.screenX},${this.y=event.screenY}`)
+          
+      }
+    },
+  }).mount("#app");
